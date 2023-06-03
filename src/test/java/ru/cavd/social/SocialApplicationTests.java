610f9c4898de771f1bb8d0034f62c0c3cb2813c7
AVC <workgormol@gmail.com>
2023-06-03 14:07:16 +0300

    init

---
+++ src/test/java/ru/cavd/social/SocialApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.cavd.social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
